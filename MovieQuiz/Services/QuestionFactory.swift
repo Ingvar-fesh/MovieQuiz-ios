893f69b7a38ca20009dd8bf4db818dc3e551c046
Ingvar-fesh <igor777.fesh@mail.ru>
2023-01-07 18:05:04 +0700

    Fixed all mistakes

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 
-class QuestionFactory: QuestionFactoryProtocol {
+final class QuestionFactory: QuestionFactoryProtocol {
     
     private let moviesLoader: MoviesLoading
-    weak var delegate: QuestionFactoryDelegate?
+    private weak var delegate: QuestionFactoryDelegate?
     private var movies: [MostPopularMovie] = []
     
     init (moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate?) {
